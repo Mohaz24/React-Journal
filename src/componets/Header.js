f7a9a51421928fc121f43c1621ffacc79573df3f
Mohammed <Mohazxsk@gmail.com>
2023-02-01 15:06:58 +1100

    setting up componets

---
+++ src/componets/Header.js
@@ -0,0 +1,10 @@
+import React from "react"
+
+export default function Header(){
+    return(
+        <div id="container">
+        <img src="../images/Fill.png" alt="" />
+        <span className="header---title">my travel journal.</span>
+        </div>
+    )
+}
