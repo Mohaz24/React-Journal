cbecf1ee8cb9a5b95e0412df6c36f99cfaf2951c
Mohammed <Mohazxsk@gmail.com>
2023-02-01 15:13:21 +1100

    complete setting up componets

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import Header from "./componets/Header"
+import Journal from "./componets/Journal"
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <Journal />
     </div>
   );
 }
